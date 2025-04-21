ea390a6660f61b30ffab39d8b2a7a60dfdf4689a
Muhammadazizlite <92134561+Muhammadazizlite@users.noreply.github.com>
2025-04-21 18:31:16 +0500

    Muhammadaziz

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,21 @@
 import fs from "fs"
 let baza=JSON.parse(fs.readFileSync("./database/data.json","utf-8"))
 function GET(req,res){
-
+      const par=req.params
+      try{
+            if(par){
+                  throw Error("Invalid id")
+            }
+            if(!baza.some(book=>book.id==par)){
+                  throw Error("Id not found")
+            }
+            res.status(200)
+            res.send(JSON.stringify(baza.filter(book=>book.id==par)[0]))
+      }
+      catch(error){
+            res.status(400)
+            res.send(error.message)
+      }
 }
 
 function PUT(req,res){
@@ -9,7 +23,22 @@ function PUT(req,res){
 }
 
 function DELETE(req,res){
-
+      const par=req.params
+      try{
+            if(par){
+                  throw Error("Invalid id")
+            }
+            if(!baza.some(book=>book.id==par)){
+                  throw Error("Id not found")
+            }
+            res.status(200)
+            res.send(JSON.stringify(baza.filter(book=>book.id==par)[0]))
+            fs.writeFileSync("./database/data.json",JSON.stringify(data.filter(book=>book.id!=par)))
+      }
+      catch(error){
+            res.status(400)
+            res.send(error.message)
+      }
 }
 function POST(req,res){
 
