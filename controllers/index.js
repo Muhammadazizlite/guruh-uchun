75af5f02c32e59962ea99a3d14f42a2436f59cbf
Muhammadazizlite <92134561+Muhammadazizlite@users.noreply.github.com>
2025-04-21 18:02:24 +0500

    shu ham kerak

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,18 @@
 import fs from "fs"
 let baza=JSON.parse(fs.readFileSync("./database/data.json","utf-8"))
 function GET(req,res){
-      res.send("salom getdan")
+
+}
+
+function PUT(req,res){
+
+}
+
+function DELETE(req,res){
+
+}
+function POST(req,res){
+      
 }
 
 export default{
